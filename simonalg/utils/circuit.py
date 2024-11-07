7242b49a130f1581695a545ff4b45616f5391b27
Oskar Mayer <oskar.mayer@gmx.at>
2024-11-07 15:28:28 +0100

    feat: add reverse mcx chain

--- simonalg/utils/circuit.py
+++ simonalg/utils/circuit.py
@@ -10,6 +10,7 @@ def x_gate_where_bitstring_is_0(circuit, register, bitstring):
             if bit == '0':
                 circuit.x(register[register.size - s_index - 1])
 
+
 def mcx_halfchain(circuit, input_register, ancilla_register):
     """
     Parameters:
@@ -22,8 +23,25 @@ def mcx_halfchain(circuit, input_register, ancilla_register):
     qubit and the last ancilliary qubit as controls, the effect is as if the CCX gate was controlled by all input qubits.
     """
     if input_register.size <= 2:
-         return
+        return
     
     circuit.ccx(input_register[0], input_register[1], ancilla_register[0])
     for i in range(2, input_register.size - 1):
-         circuit.ccx(input_register[i], ancilla_register[i - 2], ancilla_register[i - 1])
+        circuit.ccx(input_register[i], ancilla_register[i - 2], ancilla_register[i - 1])
+
+
+def reverse_mcx_halfchain(circuit, input_register, ancilla_register):
+    """
+    Parameters:
+        - circuit is the quantum circuit currently being worked on
+        - input_register is the register holding (all) control qubits for MCX
+        - ancilla_register holds ancilla qubits for MCX, it is assumed that we have exactly one fewer ancilla qubit than
+            control qubits
+    Reverses the effects of the mcx_halfchain circuit.
+    """
+    if input_register.size <= 2:
+        return
+    
+    for i in range(input_register.size - 2, 1, -1):
+        circuit.ccx(input_register[i], ancilla_register[i - 2], ancilla_register[i - 1])
+    circuit.ccx(input_register[0], input_register[1], ancilla_register[0])
