86ef11b5eeecf37041dcb317ca282d53fd8a2743
Oskar Mayer <oskar.mayer@gmx.at>
2024-11-08 16:53:14 +0100

    feat: reworked Simon's oracle implementation

--- simonalg/oracle.py
+++ simonalg/oracle.py
@@ -1,8 +1,5 @@
-import math
-from qiskit import QuantumCircuit, QuantumRegister, AncillaRegister
-
 from .utils.grouptheory import generate_group_by_order, generate_cosets_for_subgroup
-from .utils.circuit import x_gate_where_bitstring_is_0
+from .utils.circuit import x_gate_where_bitstring_is_0, optimized_mcx, generate_circuit_setup
 
 
 class SimonOracle:
@@ -21,23 +18,18 @@ class SimonOracle:
         Generates a circuit implementing the oracle. If f is the oracle function, |x> is an n-qubit register and |y> is an m-qubit register,
         the circuit performs the calculation |x>|y> -> |x>|y XOR f(y)>.
         """
-        input_register = QuantumRegister(self._n, 'x')
-        output_register_size = math.floor(math.log2((2 ** self._n) // self._hidden_subgroup_order))
-        output_register = QuantumRegister(output_register_size, 'y')
-        ancilla_register = AncillaRegister(self._n - 2, 'anc')
-
         group = generate_group_by_order(self._n)
         cosets = generate_cosets_for_subgroup(group, self._hidden_subgroup)
         
-        circuit = QuantumCircuit(input_register, output_register, ancilla_register)
+        circuit, input_register, output_register, ancilla_register = generate_circuit_setup(self._n, len(self._hidden_subgroup))
         for c_index, coset in enumerate(cosets[1:]):
             for bitstring in coset:
-                x_gate_where_bitstring_is_0(circuit, input_register, bitstring)
+                output_value = format(c_index + 1, f'0{output_register.size}b')
 
-                output_value = format(c_index + 1, f'0{output_register_size}b')
-                for o_index, bit in enumerate(output_value):
-                    if bit == '1':
-                        circuit.mcx(input_register, output_register[o_index], mode='v-chain', ancilla_qubits=ancilla_register)
+                x_gate_where_bitstring_is_0(circuit, input_register, bitstring)
+                
+                target_qubits = [output_register[i] for i in filter(lambda o_index: output_value[o_index] == '1', range(output_register.size))]
+                optimized_mcx(circuit, input_register, ancilla_register, target_qubits)
 
                 x_gate_where_bitstring_is_0(circuit, input_register, bitstring)
                 
