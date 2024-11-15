19cec4f572899f9cef819d2ce1a76ad58a31aa22
Oskar Mayer <oskar.mayer@gmx.at>
2024-11-15 16:50:06 +0100

    feat: add blocking clause implementation

--- simonalg/simon_circuit.py
+++ simonalg/simon_circuit.py
@@ -8,6 +8,10 @@ class SimonCircuit():
 
 
     def generate_standard_simon_circuit(self):
+        """
+        Generates the quantum circuit proposed by Simon in https://epubs.siam.org/doi/abs/10.1137/S0097539796298637.
+        """
+
         circuit = self.circuit_wrapper.circuit
         input_register = self.circuit_wrapper.input_register
 
@@ -16,3 +20,36 @@ class SimonCircuit():
         self._oracle.apply_to_circuit(self.circuit_wrapper)
 
         circuit.h(input_register)
+
+
+    def add_blocking_clauses_for_bitstrings(self, bitstrings):
+        """
+        Parameters:
+            - bitstrings are the bitstrings we would like to remove from a superposition. All elements
+              are assumed to be non-zero and of equal length as the bitstrings of the oracle's hidden subgroup.
+        Modifies the circuit according to https://ieeexplore.ieee.org/abstract/document/595153, Lemma 7.
+        """
+        for blocking_index, bitstring in enumerate(bitstrings):
+            self.add_blocking_clause_for_bitstring(bitstring, blocking_index)
+
+
+    def add_blocking_clause_for_bitstring(self, bitstring, blocking_index):
+        """
+        Parameters:
+            - bitstring is the bitstring we would like to remove from a superposition. bitstring is assumed to be
+              non-zero and of equal length as the bitstrings of the oracle's hidden subgroup.
+        Modifies the circuit according to https://ieeexplore.ieee.org/abstract/document/595153, Lemma 6.
+        """
+        indices_where_bitstring_is_1 = list(filter(lambda i: bitstring[i] == '1', range(len(bitstring))))
+        j = indices_where_bitstring_is_1[0]
+
+        circuit, input_register, _, blockingclause_register, _ = self.circuit_wrapper.get()
+
+        blocking_qubit = blockingclause_register[len(blockingclause_register) - 1 - blocking_index]
+
+        circuit.cx(input_register[len(input_register) - 1 - j], blocking_qubit)
+        for i in indices_where_bitstring_is_1:
+            circuit.cx(blocking_qubit, input_register[len(input_register) - 1 - i])
+
+        circuit.h(blocking_qubit)
+ 
