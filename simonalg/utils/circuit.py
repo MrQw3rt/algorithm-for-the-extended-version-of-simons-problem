9f16a30c19df80ed156af1bd4ce2d47e26ad68ed
Oskar Mayer <oskar.mayer@gmx.at>
2024-11-16 11:48:47 +0100

    fix: docstrings

--- simonalg/utils/circuit.py
+++ simonalg/utils/circuit.py
@@ -55,10 +55,9 @@ def x_gate_where_bitstring_is_0(circuit, register, bitstring):
 def mcx_halfchain(circuit, input_register, ancilla_register):
     """
     Parameters:
-        - circuit is the quantum circuit currently being worked on
-        - input_register is the register holding (all) control qubits for MCX
-        - ancilla_register holds ancilla qubits for MCX, it is assumed that we have exactly one fewer ancilla qubit than
-          control qubits
+        - circuit is the quantum circuit currently being worked on.
+        - input_register is the register holding (all) control qubits for MCX.
+        - ancilla_register holds ancilla qubits for MCX, it is assumed that we have at least two fewer ancilla qubits than input qubits.
     For an n-qubit input, the halfchain circuit prepares the first (n-1) steps for an n-qubit controlled X gate. The gate is
     described in Lemma 13 of the thesis. After the halfchain circuit was applied, when we apply a CCX gate with the last input
     qubit and the last ancilliary qubit as controls, the effect is as if the CCX gate was controlled by all input qubits.
@@ -68,8 +67,8 @@ def mcx_halfchain(circuit, input_register, ancilla_register):
     if input_register_size <= 2:
         return
     
-    if ancilla_register_size + 2 != input_register.size:
-        raise Exception(f'Input register size {input_register_size} is not ancilla register size ({ancilla_register_size}) + 2')
+    if ancilla_register_size + 2 < input_register.size:
+        raise Exception(f'Ancilla register is too small: an input register of size {input_register_size} requires at least {input_register_size} + 2 ancilla qubits!')
 
     circuit.ccx(input_register[0], input_register[1], ancilla_register[0])
     for i in range(2, input_register.size - 1):
@@ -79,10 +78,9 @@ def mcx_halfchain(circuit, input_register, ancilla_register):
 def reverse_mcx_halfchain(circuit, input_register, ancilla_register):
     """
     Parameters:
-        - circuit is the quantum circuit currently being worked on
-        - input_register is the register holding (all) control qubits for MCX
-        - ancilla_register holds ancilla qubits for MCX, it is assumed that we have exactly one fewer ancilla qubit than
-            control qubits
+        - circuit is the quantum circuit currently being worked on.
+        - input_register is the register holding (all) control qubits for MCX.
+        - ancilla_register holds ancilla qubits for MCX, it is assumed that we have at least two fewer ancilla qubits than input qubits.
     Reverses the effects of the mcx_halfchain circuit.
     """
     if input_register.size <= 2:
@@ -96,11 +94,11 @@ def reverse_mcx_halfchain(circuit, input_register, ancilla_register):
 def optimized_mcx(circuit, input_register, ancilla_register, target_qubits):
     """
     Parameters:
-        - circuit is the quantum circuit currently being worked on
-        - input_register is the register holding (all) control qubits for MCX
+        - circuit is the quantum circuit currently being worked on.
+        - input_register is the register holding (all) control qubits for MCX.
         - ancilla_register holds ancilla qubits for MCX, all ancilla qubits are assumed to be in state |0>.
           We assume that there is exactly one fewer ancilla qubit than input qubits.
-        - target_qubits are the the target qubits for MCX
+        - target_qubits are the the target qubits for MCX.
     Executes MCX for each target_qubit where the control qubits are all qubits in input_register. 
     """
     mcx_halfchain(circuit, input_register, ancilla_register)
@@ -122,8 +120,8 @@ def optimized_mcx(circuit, input_register, ancilla_register, target_qubits):
 def conditional_phase_shift_by_index(circuit, input_register, ancilla_register, index):
     """
     Parameters:
-        - circuit is the quantum circuit currently being worked on
-        - input_register is the register holding inputs (e.g. for a Simon oracle)
+        - circuit is the quantum circuit currently being worked on.
+        - input_register is the register holding inputs (e.g. for a Simon oracle).
         - ancilla_register holds at least one qubit.
         - index is the index of the input register, which we use to decide whether to perform the phase shift or not.
     Implements the operater S_A from https://ieeexplore.ieee.org/abstract/document/595153, Lemma 8, where the operator
