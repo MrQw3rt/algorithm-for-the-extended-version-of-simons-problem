44392c886c999b766aebd857ae5ebc3701b105f6
Oskar Mayer <oskar.mayer@gmx.at>
2024-11-22 14:37:06 +0100

    feat: additional test cases for standard Simon's algorithm

--- test/test_simon_standard.py
+++ test/test_simon_standard.py
@@ -23,6 +23,21 @@ class SimonStandardTest(unittest.TestCase):
             self.assertEqual(blockingclause_register_state, '0' * len(blockingclause_register))
 
 
+    def run_circuit_and_assert_it_is_its_own_inverse(self, hidden_subgroup):
+        oracle = SimonOracle(hidden_subgroup)
+        simon_circuit = SimonCircuit(oracle)
+
+        circuit = simon_circuit.generate_standard_simon_circuit()
+        input_register, output_register, blockingclause_register, ancilla_register = simon_circuit.circuit_wrapper.get_registers()
+        result = run_circuit(circuit.compose(circuit), [input_register, output_register, blockingclause_register, ancilla_register])
+        
+        self.assertIs(len(result), 1)
+        state = list(result.keys())[0]
+
+        state_pure = state.replace(' ', '')
+        self.assertEqual(state_pure, '0' * len(state_pure))
+
+
     def test_simonalg_two_qubits_hsgorder_1(self):
         hidden_subgroup = ['00']
         self.run_circuit_and_assert_all_measurments_are_in_hbot(hidden_subgroup)
@@ -31,6 +46,11 @@ class SimonStandardTest(unittest.TestCase):
     def test_simonalg_two_qubits_hsgorder_two_1(self):
         hidden_subgroup = ['00', '01']
         self.run_circuit_and_assert_all_measurments_are_in_hbot(hidden_subgroup)
+
+
+    def test_simonalg_two_qubits_hsgorder_two_1a(self):
+        hidden_subgroup = ['00', '01']
+        self.run_circuit_and_assert_it_is_its_own_inverse(hidden_subgroup)
         
 
     def test_simonalg_two_qubits_hsgorder_two_2(self):
@@ -38,14 +58,29 @@ class SimonStandardTest(unittest.TestCase):
         self.run_circuit_and_assert_all_measurments_are_in_hbot(hidden_subgroup)
 
 
+    def test_simonalg_two_qubits_hsgorder_two_2a(self):
+        hidden_subgroup = ['00', '10']
+        self.run_circuit_and_assert_it_is_its_own_inverse(hidden_subgroup)
+
+
     def test_simonalg_two_qubits_hsgorder_two_3(self):
         hidden_subgroup = ['00', '11']
         self.run_circuit_and_assert_all_measurments_are_in_hbot(hidden_subgroup)
+
+    
+    def test_simonalg_two_qubits_hsgorder_two_3a(self):
+        hidden_subgroup = ['00', '11']
+        self.run_circuit_and_assert_it_is_its_own_inverse(hidden_subgroup)
 
 
     def test_simonalg_three_qubits_hsgorder_two_1(self):
         hidden_subgroup = ['000', '010']
         self.run_circuit_and_assert_all_measurments_are_in_hbot(hidden_subgroup)
+
+    
+    def test_simonalg_three_qubits_hsgorder_two_1a(self):
+        hidden_subgroup = ['000', '010']
+        self.run_circuit_and_assert_it_is_its_own_inverse(hidden_subgroup)
 
 
     def test_simonalg_three_qubits_hsgorder_two_2(self):
@@ -53,17 +88,37 @@ class SimonStandardTest(unittest.TestCase):
         self.run_circuit_and_assert_all_measurments_are_in_hbot(hidden_subgroup)
 
 
+    def test_simonalg_three_qubits_hsgorder_two_2a(self):
+        hidden_subgroup = ['000', '011']
+        self.run_circuit_and_assert_it_is_its_own_inverse(hidden_subgroup)
+
+
     def test_simonalg_three_qubits_hsgorder_four_1(self):
         hidden_subgroup = ['000', '100', '010', '110']
         self.run_circuit_and_assert_all_measurments_are_in_hbot(hidden_subgroup)
+
+
+    def test_simonalg_three_qubits_hsgorder_four_1a(self):
+        hidden_subgroup = ['000', '100', '010', '110']
+        self.run_circuit_and_assert_it_is_its_own_inverse(hidden_subgroup)
 
 
     def test_simonalg_three_qubits_hsgorder_four_2(self):
         hidden_subgroup = ['000', '100', '111', '011']
         self.run_circuit_and_assert_all_measurments_are_in_hbot(hidden_subgroup)
 
+
+    def test_simonalg_three_qubits_hsgorder_four_2a(self):
+        hidden_subgroup = ['000', '100', '111', '011']
+        self.run_circuit_and_assert_it_is_its_own_inverse(hidden_subgroup)
+
     
     def test_simonalg_three_qubits_hsgorder_eight_1(self):
         hidden_subgroup = ['000', '001', '010', '011', '100', '101', '110', '111']
         self.run_circuit_and_assert_all_measurments_are_in_hbot(hidden_subgroup)
+
+
+    def test_simonalg_three_qubits_hsgorder_eight_1a(self):
+        hidden_subgroup = ['000', '001', '010', '011', '100', '101', '110', '111']
+        self.run_circuit_and_assert_it_is_its_own_inverse(hidden_subgroup)
 
