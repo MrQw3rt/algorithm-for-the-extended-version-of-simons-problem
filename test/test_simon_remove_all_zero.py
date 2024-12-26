a035029332466e9cb2065eb35aae0d8e5ca95c82
Oskar Mayer <oskar.mayer@gmx.at>
2024-12-26 19:48:51 +0100

    refactor: make test utils reusable

--- test/test_simon_remove_all_zero.py
+++ test/test_simon_remove_all_zero.py
@@ -1,226 +1,117 @@
 import unittest
-import numpy as np
 
-from functools import reduce
-
-from simonalg.oracle import SimonOracle
-from simonalg.simon_circuit import SimonCircuit
-
-from utils import run_circuit, run_circuit_without_measurement
-from simonalg.utils.grouptheory import generate_group_by_order, generate_orthogonal_group, is_in_orthogonal_group
+from utils import construct_extended_simon_circuit_and_run_for_every_possible_index, BaseAssertions
+from simonalg.utils.grouptheory import generate_group_by_order
 
 
-def format_statevector(sv, input_register, output_register):
-    def format_item(item):
-        state = item[0]
-        amplitude = item[1]
-        return f'|{state[-len(input_register):]}>|{state[-len(input_register) - len(output_register):-len(input_register)]}>', amplitude
-
-    results = [format_item(item) for item in sv.items()]
-    results.sort(key=lambda tuple: tuple[0])
-    results = list(filter(lambda t: np.absolute(t[1]) > 1e-8, results))
-
-    results_string = '\n'.join([f'\t{t[0]}: {t[1]}' for t in results])
-    return results_string
-
-
-def log_statevectors(result, input_register, output_register):
-    strings = [(t[0], format_statevector(t[1].to_dict(), input_register, output_register)) for t in result.items()]
-    strings.sort(key=lambda t: t[0])
-    
-    strings = [f'{t[0]}\n{t[1]}' for t in strings]
-    res = '\n'.join(strings)
-    print()
-    print('Statevectors:')
-    print(res)
-
-
-class SimonRemoveAllZeroTest(unittest.TestCase):
-
-    def construct_circuit(self, hidden_subgroup, index):
-        oracle = SimonOracle(hidden_subgroup)
-        simon_circuit = SimonCircuit(oracle)
-
-        input_register, output_register, _, _ = simon_circuit.circuit_wrapper.get_registers()
-        rmz_circuit = simon_circuit.generate_remove_zero_circuit([], index)
-        return rmz_circuit, input_register, output_register
-
-    def construct_circuit_and_run(self, hidden_subgroup, index, orthogonal_subgroup, log_circuit=False):
-        rmz_circuit, input_register, _ = self.construct_circuit(hidden_subgroup, index)
-        print()
-        print()
-        print('Running circuit for hidden subgroup ', hidden_subgroup)
-        print('Orthogonal group is ', orthogonal_subgroup)
-        print('Index is ', index)
-        if log_circuit:
-            print('Generated Circuit:')
-            print(rmz_circuit)
-
-        result = run_circuit(rmz_circuit, [input_register])
-        print('Results: ', result)
-
-        return list(result.keys())
-
-
-    def assert_all_zero_not_measured(self, measurements):
-        n = len(measurements[0])
-        allzero = '0' * n
-        self.assertTrue(all([bitstring != allzero for bitstring in measurements]))
-
-
-    def assert_all_measurements_have_1_at_index(self, measurements, index):
-        n = len(measurements[0])
-        self.assertTrue(all([bitstring[n - index - 1] == '1' for bitstring in measurements]))
-
-
-    def assert_all_measurements_are_in_orthogonal_group(self, measurements, hidden_subgroup):
-        self.assertTrue(all([is_in_orthogonal_group(bitstring, hidden_subgroup) for bitstring in measurements]))
-
-
-    def run_circuit_for_each_index_that_can_be_1(self, hidden_subgroup, log_circuit_and_statevector=False):
-        n = len(hidden_subgroup[0])
-        group = generate_group_by_order(n)
-        orthogonal_subgroup = generate_orthogonal_group(group, hidden_subgroup)
-
-        def map_to_indices_with_1(bitstring):
-            return set(filter(lambda index: bitstring[n - index - 1] == '1', range(0, n)))
-        orthogonal_group_mapped = [map_to_indices_with_1(bitstring) for bitstring in orthogonal_subgroup]
-        indices_that_can_be_1 = reduce(lambda a,b: a.union(b), orthogonal_group_mapped, set())
-        for index in indices_that_can_be_1:
-            measurements = self.construct_circuit_and_run(hidden_subgroup, index, orthogonal_subgroup, log_circuit=log_circuit_and_statevector)
-            if log_circuit_and_statevector:
-                sv_circuit, sv_input_register, sv_output_register = self.construct_circuit(hidden_subgroup, index)
-                result_sv = run_circuit_without_measurement(sv_circuit).data(0)
-                log_statevectors(result_sv, sv_input_register, sv_output_register)
-
-            
-            self.assert_all_zero_not_measured(measurements)
-            self.assert_all_measurements_have_1_at_index(measurements, index)
-            self.assert_all_measurements_are_in_orthogonal_group(measurements, hidden_subgroup)
-
-    
-    def run_circuit_for_all_zero_orthogonal_group(self, hidden_subgroup, log_circuit_and_statevector=False):
-        n = len(hidden_subgroup[0])
-        group = generate_group_by_order(n)
-        orthogonal_subgroup = generate_orthogonal_group(group, hidden_subgroup)
-
-        indices_that_can_be_1 = range(n)
-        for index in indices_that_can_be_1:
-            measurements = self.construct_circuit_and_run(hidden_subgroup, index, orthogonal_subgroup, log_circuit=log_circuit_and_statevector)
-            if log_circuit_and_statevector:
-                sv_circuit, input_register, output_register = self.construct_circuit(hidden_subgroup, index)
-                result_sv = run_circuit_without_measurement(sv_circuit).data(0)
-                log_statevectors(result_sv, input_register, output_register)
-
-            self.assertTrue(len(measurements) == 1)
-            self.assertTrue(measurements[0] == n * '0')
+class SimonRemoveAllZeroTest(BaseAssertions):
 
 
     def test_remove_all_zero_vector_for_two_qubits_0(self):
         # Case oracle is bijective
         hidden_subgroup = ['00']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_two_qubits_1(self):
         hidden_subgroup = ['00', '01']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup, log_circuit_and_statevector=True)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index, log_circuit_and_statevector=True)
 
 
     def test_remove_all_zero_vector_for_two_qubits_2(self):
         hidden_subgroup = ['00', '10']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
     
     def test_remove_all_zero_vector_for_two_qubits_3(self):
         # Case hidden subgroup is entire group G
         hidden_subgroup = generate_group_by_order(2)
-        self.run_circuit_for_all_zero_orthogonal_group(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_only_all_zero_is_measured)
 
 
     def test_remove_all_zero_vector_for_three_qubits_0(self):
         # Case oracle is bijective
         hidden_subgroup = ['000']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_three_qubits_1(self):
         hidden_subgroup = ['000', '001']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
     
     def test_remove_all_zero_vector_for_three_qubits_2(self):
         hidden_subgroup = ['000', '010']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_three_qubits_3(self):
         hidden_subgroup = ['000', '100']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_three_qubits_4(self):
         hidden_subgroup = ['000', '111']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_three_qubits_5(self):
         # Case hidden subgroup is entire group G
         hidden_subgroup = generate_group_by_order(3)
-        self.run_circuit_for_all_zero_orthogonal_group(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_only_all_zero_is_measured)
 
 
     def test_remove_all_zero_vector_for_four_qubits_0(self):
         # Case oracle is bijective
         hidden_subgroup = ['0000']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_four_qubits_1(self):
         hidden_subgroup = ['0000', '1100']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_four_qubits_2(self):
         hidden_subgroup = ['0000', '1001']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_four_qubits_3(self):
         hidden_subgroup = ['0000', '1101']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_four_qubits_4(self):
         hidden_subgroup = ['0000', '1111']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_four_qubits_5(self):
         # Case hidden subgroup is entire group G
         hidden_subgroup = generate_group_by_order(4)
-        self.run_circuit_for_all_zero_orthogonal_group(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_only_all_zero_is_measured)
 
 
     @unittest.skip('This takes too long to simulate on a laptop!')
     def test_remove_all_zero_vector_for_five_qubits_0(self):
         # Case oracle is bijective
         hidden_subgroup = ['00000']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_five_qubits_1(self):
         hidden_subgroup = ['00000', '00001', '00010', '00011']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_five_qubits_2(self):
         hidden_subgroup = ['00000', '10000', '01000', '11000']
-        self.run_circuit_for_each_index_that_can_be_1(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_all_zero_not_measured_and_all_results_have_1_at_target_index)
 
 
     def test_remove_all_zero_vector_for_five_qubits_3(self):
         # Case hidden subgroup is entire group G
         hidden_subgroup = generate_group_by_order(5)
-        self.run_circuit_for_all_zero_orthogonal_group(hidden_subgroup)
+        construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, [], self.assert_only_all_zero_is_measured)
 
--- test/utils.py
+++ test/utils.py
@@ -1,5 +1,46 @@
+import numpy as np
 from qiskit import ClassicalRegister, transpile
 from qiskit_aer import AerSimulator
+
+import unittest
+from functools import reduce
+
+from simonalg.oracle import SimonOracle
+from simonalg.simon_circuit import SimonCircuit
+from simonalg.utils.grouptheory import generate_group_by_order, generate_orthogonal_group, is_in_orthogonal_group
+
+
+def log_parameters(params):
+    print()
+    print()
+    for message, object in params:
+        print(message, object)
+
+
+def format_statevector(sv, input_register, output_register):
+    def format_item(item):
+        state = item[0]
+        amplitude = item[1]
+        return f'|{state[-len(input_register):]}>|{state[-len(input_register) - len(output_register):-len(input_register)]}>', amplitude
+
+    results = [format_item(item) for item in sv.items()]
+    results.sort(key=lambda tuple: tuple[0])
+    results = list(filter(lambda t: np.absolute(t[1]) > 1e-8, results))
+
+    results_string = '\n'.join([f'\t{t[0]}: {t[1]}' for t in results])
+    return results_string
+
+
+def log_statevectors(result, input_register, output_register):
+    strings = [(t[0], format_statevector(t[1].to_dict(), input_register, output_register)) for t in result.items()]
+    strings.sort(key=lambda t: t[0])
+    
+    strings = [f'{t[0]}\n{t[1]}' for t in strings]
+    res = '\n'.join(strings)
+    print()
+    print('Statevectors:')
+    print(res)
+
 
 def run_circuit(circuit, measured_registers):
     for register in reversed(measured_registers):   # Reversed in order to preserve the register order in the output
@@ -20,3 +61,100 @@ def run_circuit_without_measurement(circuit):
     transpiled_circuit = transpile(circuit, simulator)
 
     return simulator.run(transpiled_circuit).result()
+
+
+def construct_extended_simon_circuit(hidden_subgroup, index, blockingclause_bitstrings=[]):
+    oracle = SimonOracle(hidden_subgroup)
+    simon_circuit = SimonCircuit(oracle)
+
+    input_register, output_register, _, _ = simon_circuit.circuit_wrapper.get_registers()
+    rmz_circuit = simon_circuit.generate_remove_zero_circuit(blockingclause_bitstrings, index)
+    return rmz_circuit, input_register, output_register
+
+
+def construct_and_run_extended_simon_circuit(hidden_subgroup, index, orthogonal_subgroup, blockingclause_bitstrings=[], log_circuit=False):
+    rmz_circuit, input_register, _ = construct_extended_simon_circuit(hidden_subgroup, index, blockingclause_bitstrings=blockingclause_bitstrings)
+    log_parameters([
+        ('Running circuit for hidden subgroup ', hidden_subgroup),
+        ('Orthogonal group is ', orthogonal_subgroup),
+        ('Index is ', index)
+    ] + ([('Generated Circuit\n', rmz_circuit)] if log_circuit else []))
+
+    result = run_circuit(rmz_circuit, [input_register])
+    print('Results: ', result)
+
+    return list(result.keys())
+
+
+def find_indices_that_can_be_1(orthogonal_subgroup):
+        n = len(orthogonal_subgroup[0])
+        def map_to_indices_with_1(bitstring):
+            return set(filter(lambda index: bitstring[n - index - 1] == '1', range(0, n)))
+        orthogonal_group_mapped = [map_to_indices_with_1(bitstring) for bitstring in orthogonal_subgroup]
+        return reduce(lambda a,b: a.union(b), orthogonal_group_mapped, set())
+
+
+def construct_extended_simon_circuit_and_run_for_every_possible_index(hidden_subgroup, blockingclause_bitstrings, assert_fn, log_circuit_and_statevector=False):
+    n = len(hidden_subgroup[0])
+    group = generate_group_by_order(n)
+    orthogonal_subgroup = generate_orthogonal_group(group, hidden_subgroup)
+
+    all_indices_that_can_be_1 = find_indices_that_can_be_1(orthogonal_subgroup)
+    indices_locked_by_bitstrings = []
+    for bitstring in blockingclause_bitstrings:
+        for index in find_indices_that_can_be_1([bitstring]):
+            if len(indices_locked_by_bitstrings) == 0 or indices_locked_by_bitstrings[-1] < index:
+                indices_locked_by_bitstrings.append(index)
+                break
+    indices_that_can_be_1_after_bitstrings = list(set(all_indices_that_can_be_1).difference(set(indices_locked_by_bitstrings)))
+    if len(indices_that_can_be_1_after_bitstrings) == 0:
+        indices_that_can_be_1_after_bitstrings = range(n)
+
+    for index in indices_that_can_be_1_after_bitstrings:
+        measurements = construct_and_run_extended_simon_circuit(hidden_subgroup, index, orthogonal_subgroup, blockingclause_bitstrings=blockingclause_bitstrings)
+        if log_circuit_and_statevector:
+            sv_circuit, sv_input_register, sv_output_register = construct_extended_simon_circuit(hidden_subgroup, index)
+            result_sv = run_circuit_without_measurement(sv_circuit).data(0)
+            log_statevectors(result_sv, sv_input_register, sv_output_register)
+
+        params = { 
+            'measurements': measurements, 
+            'index': index, 
+            'hidden_subgroup': hidden_subgroup, 
+            'blockingclause_bitstrings': blockingclause_bitstrings 
+        }
+        assert_fn(params)
+
+
+class BaseAssertions(unittest.TestCase):
+
+    def assert_all_zero_not_measured(self, measurements):
+        n = len(measurements[0])
+        allzero = '0' * n
+        self.assertTrue(all([bitstring != allzero for bitstring in measurements]))
+
+
+    def assert_all_measurements_have_1_at_index(self, measurements, index):
+        n = len(measurements[0])
+        self.assertTrue(all([bitstring[n - index - 1] == '1' for bitstring in measurements]))
+
+
+    def assert_all_measurements_are_in_orthogonal_group(self, measurements, hidden_subgroup):
+        self.assertTrue(all([is_in_orthogonal_group(bitstring, hidden_subgroup) for bitstring in measurements]))
+
+
+    def assert_all_zero_not_measured_and_all_results_have_1_at_target_index(self, params):
+        measurements = params['measurements']
+        index = params['index']
+        hidden_subgroup = params['hidden_subgroup']
+
+        self.assert_all_zero_not_measured(measurements)
+        self.assert_all_measurements_have_1_at_index(measurements, index)
+        self.assert_all_measurements_are_in_orthogonal_group(measurements, hidden_subgroup)
+
+
+    def assert_only_all_zero_is_measured(self, params):
+        measurements = params['measurements']
+        n = len(measurements[0])
+        self.assertTrue(len(measurements) == 1)
+        self.assertTrue(measurements[0] == n * '0')
