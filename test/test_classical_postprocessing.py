0206a9920b85232ed57f76c86906759813c92888
Oskar Mayer <oskar.mayer@gmx.at>
2025-03-12 15:50:29 +0100

    fix: pylint errors in test_classical_postprocessing.py

--- test/test_classical_postprocessing.py
+++ test/test_classical_postprocessing.py
@@ -16,7 +16,7 @@ class PostProcessingTest(unittest.TestCase):
         res = convert_to_basis_of_hidden_subgroup(['010', '100'], 3)
         self.assertListEqual(res, ['001'])
 
-    
+
     def test_hidden_subgroup_size_4(self):
         # Hidden subgroup is ['000', '001', '010', '011']
         res = convert_to_basis_of_hidden_subgroup(['100'], 3)
